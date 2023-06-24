25761ca980ab0ff92a811963df94431a9bc1a38a
曹建涛 <13437104137@163.com>
2023-06-24 11:43:26 +0800

    Update SSOUtils.js

--- src/util/SSOUtils.js
+++ src/util/SSOUtils.js
@@ -1,5 +1,5 @@
 const toLogin = () => {
-  location.href = `http://api.caojiantao.site:38080/login.html?redirectUrl=${location.href}`;
+  location.href = `http://api.caojiantao.site:1024/login.html?redirectUrl=${location.href}`;
 };
 
 export { toLogin };
